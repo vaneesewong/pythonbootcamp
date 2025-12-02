6403169bd7a9d38e313e0f144bccc14947b66d8e
vaneesewong <vaneesewsh@gmail.com>
2025-12-02 10:31:28 +0800

    first commit

---
+++ py/01.py
@@ -0,0 +1,30 @@
+name = "Van"         # String 
+age = 27             # Integer
+weight = 20.5        # Float
+is_female = True    # Boolean 
+
+print(name)
+print(type(name))
+
+print(age)
+print(type(age))
+
+
+x = 10
+y = 3
+
+print(x + y)    # Addition
+print(x - y)    # Subtraction
+print (x * y)   # Multiplication
+print (x / y)   # Division
+print(x // y)   # Floor division
+print(x % y)    # Modulus
+print (x ** y)  # Exponentiation 
+
+
+# Ex 2: Convert Celcius to Fahrenheit #
+C = 25
+F = C * 9/5 + 32
+
+print(F)
+
